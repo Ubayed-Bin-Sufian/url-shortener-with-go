96659faf80581a3ee4daab7257aabb877f0ab28d
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-27 23:20:54 +0600

    Implement ResolveURL function for short URL redirection and error handling

---
+++ api/routes/resolve.go
@@ -0,0 +1,35 @@
+// When a user uses the short url, we check the redis database for the short url 
+// If it exists, we redirect to the original URL
+
+package routes
+
+import (
+	"github.com/ubayed-bin-sufian/shorten-url-fiber-redis/database"
+	"github.com/gofiber/fiber/v2"
+	"github.com/go-redis/redis/v8"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+
+	url := c.Params("url")
+
+	r := database.CreateClient()
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "short URL not found in the database",
+		})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalError).JSON(fiber.Map{
+			"error": "error retrieving short URL from the database",
+		})
+	}
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+
+	_ = rInr.Incr(database.Ctx, "counter")
+
+	return c.Redirect(value, 301)
+}
