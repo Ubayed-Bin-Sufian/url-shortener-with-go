be327e0f60e697d3d074385906c8954b3e5b078f
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-28 06:19:23 +0600

    Implement rate limiting and IP address tracking in ShortenURL function

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"time"
+	"github.com/ubayed-bin-sufian/shorten-url-fiber-redis/database"
+	"os"
 )
 
 type request struct {
@@ -29,6 +31,23 @@ func ShortenURL(c *fiber.Ctx) error {
 	}
 
 	// Implement rate limiting
+	// Check if the IP address of the user is already entered in the redis DB
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+	val, err := r2.Get(database.Ctx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		val, _ = r2.Get(database.Ctx, c.IP().Result())
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error": "Rate limit exceeded",
+				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
+			})
+		}
+	}
 
 	// Check if the input sent by the user is an actual URL
 	if !govalidator.IsURL(body.URL) {
@@ -47,4 +66,5 @@ func ShortenURL(c *fiber.Ctx) error {
 	// Enforce https, SSL
 	body.URL = helpers.EnforceSSL(body.URL)
 
+	r2.Decr(database.Ctx, c.IP())
 }
